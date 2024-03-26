6fbd38ecffc88bd803c952e6884be1487d20dacd
Rahul Toggi <rahultoggi308@gmail.com>
2024-03-26 11:39:22 +0530

    Django app

---
+++ profiles_projecct/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for profiles_projecct project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "profiles_projecct.settings")
+
+application = get_wsgi_application()
